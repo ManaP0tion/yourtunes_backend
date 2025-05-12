5ce660801dd73df03fdc5e2ca9810fc40592741f
ManaP0tion <aer0dev7@gmail.com>
2025-05-12 16:46:48 +0900

    [UPDATE] .gitignore

--- src/main/java/com/example/yourtunes_backend/Controller/TestController.java
+++ src/main/java/com/example/yourtunes_backend/Controller/TestController.java
@@ -1,4 +1,16 @@
 package com.example.yourtunes_backend.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello from spring";
+    }
 }
